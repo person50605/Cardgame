5fa5cebfeb115f59ba21dbdf388f1111b1cb85e7
person50605 <124418091+person50605@users.noreply.github.com>
2025-06-07 14:49:43 -0700

    Add cast loop
    it doesn't work

--- card_stuff.js
+++ card_stuff.js
@@ -1,1 +1,19 @@
-card = 1;
+
+
+cardindexnames = ["error","slap","punch","stab"];
+cardindexeffects = ["error","dmg:10,bleed:1","dmg:15","dmg:5,bleed:3"];
+
+
+
+hand = ["error","slap","punch","stab"];
+
+
+function cast(card) {
+  
+  i = 1
+  while hand [card] != cardindexnames[i] {
+    i++
+  }
+  
+  hand [card] = "";
+}
